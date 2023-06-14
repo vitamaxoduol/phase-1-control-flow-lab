caa5084bdb3f72727be124bdf460d305d31ff978
Vitamax Oduol <85936735+vitamaxoduol@users.noreply.github.com>
2023-06-14 12:06:38 +0300

    Completed assignmemt

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance){
+	if(distance <= 400){
+		return "This one is on me!"}
 
-function ternaryCheckCity(){
-  // Write your code here!
+	else if (distance > 2000 && distance <= 2500){
+		return "I will gladly take your thirty bucks."
+	}
+	else (distance > 2500)
+		return "No can do."
 }
+// console.log(scuberGreetingForFeet(199));
+  
+function ternaryCheckCity(city){
+  return city ==='NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+// console.log(ternaryCheckCity('NYC'));
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generous){
+	let result;
+	switch (generous){
+	case "generous":
+		result = "Thank you so much.";
+		break;
+	case "not as generous":
+		result = "Thank you.";
+		break;
+	default:
+		result = "Bye.";
+		break;
+	}
+
+	return result;
 }
+// console.log(switchOnCharmFromTip('generous'));
